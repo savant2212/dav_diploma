d3cf60d8dc72a29d57d674dd1245f5d49d238733
savant <god@savant.su>
2010-12-20 03:13:00 +0300

    added mount script

---
+++ mount_res.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+SECRET_FILE=/tmp/webdav.secret
+CONF_FILE=/tmp/webdav.conf
+
+login=`zenity --entry \
+	--title="Аутентификация" \
+	--text="Введите имя пользователя:"`
+	      
+if [ login = "" ]; then
+      	echo "Имя пользователя не введено"
+      	exit 1
+fi
+
+password=`zenity --entry \
+	--title="Аутентификация" \
+	--text="Введите пароль:" \
+	--hide-text`
+	
+if [ password = "" ]; then
+      	echo "Пароль не введён"
+      	exit 1
+fi
+
+if [ ! -e $SECRET_FILE ]; then
+	touch $SECRET_FILE
+fi
+ 
+chmod 600 $SECRET_FILE
+
+echo "$1 $login $password" > $SECRET_FILE
+
+if [ ! -e $CONF_FILE ]; then
+	touch $CONF_FILE
+fi
+ 
+chmod 600 $CONF_FILE
+echo "secrets $SECRET_FILE" > $CONF_FILE
+
+/home/savant/devel/mount.davfs $1 $2 -oconf=$CONF_FILE
+
+rm -f $CONF_FILE $SECRET_FILE
